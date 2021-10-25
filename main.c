72cfe9ee6aee5d8f2ab9c1969fbcea8631ca54ad
barohit <8manPhilosophy@localhost.localdomain>
2021-10-24 23:32:26 -0400

    overriding commiter name

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "gamemethods.h"
 
+//will be updated
 int main() {
     struct Fighter littleMac = createFighter("little mac", 1000, 100, 80, 40);
     printf("%d", littleMac.health);
